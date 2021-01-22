7907f9f93d47b4f878cc3b8f469e95dd6582e952
Guillaume Hullin <guillaume.hullin@futur-tech.com>
2021-01-22 19:53:23 +0100

    Implement config of apt into the deploy script

--- custix/scripts/os_updates.sh
+++ custix/scripts/os_updates.sh
@@ -6,10 +6,6 @@ resource=${1}
 
 OS_FAMILY=`lsb_release -s -i 2>/dev/null` 
 if [[ ${OS_FAMILY} =~ (Ubuntu|Debian) ]]; then
-    if ! [[ -f /etc/apt/apt.conf.d/02custix ]]; then
-	echo 'APT::Periodic::Enable "1";' > /etc/apt/apt.conf.d/02custix
-	echo 'APT::Periodic::Update-Package-Lists "1";' >> /etc/apt/apt.conf.d/02custix
-    fi
     if [[ ${resource} == "security" ]]; then
 	res=`apt-get -s upgrade | grep -ci ^inst.*security | tr -d '\n'`
     elif [[ ${resource} == "updates" ]]; then
--- deploy_zabbix.sh
+++ deploy_zabbix.sh
@@ -6,3 +6,10 @@ mkdir -p ${ZABBIX_DIR}/scripts/agentd/custix
 cp -rv ${SOURCE_DIR}/custix/custix_software_updates.sh            ${ZABBIX_DIR}/scripts/agentd/custix/
 cp -rv ${SOURCE_DIR}/custix/scripts              ${ZABBIX_DIR}/scripts/agentd/custix/
 cp -rv ${SOURCE_DIR}/custix/zabbix_agentd.conf   ${ZABBIX_DIR}/zabbix_agentd.d/custix_software_updates.conf
+
+# Adding conf for periodic apt update (must be run with root)
+OS_FAMILY=`lsb_release -s -i 2>/dev/null` 
+if [[ ${OS_FAMILY} =~ (Ubuntu|Debian) ]]; then
+    echo 'APT::Periodic::Enable "1";' > /etc/apt/apt.conf.d/02custix_software_updates
+    echo 'APT::Periodic::Update-Package-Lists "1";' >> /etc/apt/apt.conf.d/02custix_software_updates
+fi
